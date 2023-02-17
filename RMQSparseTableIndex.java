6ed6b1d2e2671ae99c18b21eb0ed9144dfbd924e
karna <harishgontu@gmail.com>
2023-02-16 21:34:43 -0600

    sparse table index

--- RMQSparseTableIndex.java
+++ RMQSparseTableIndex.java
@@ -1,6 +1,26 @@
 package lp1;
 
-public class RMQSparseTableIndex extends RMQSparseTable {
+public class RMQSparseTableIndex implements RMQStructure{
+
+    int n;
+    int[][] st;
+
+    protected int highestK(int n) {
+        int k = 0;
+        int k2 = 1;
+        while (k2 <= n) {
+            k++;
+            k2 *= 2;
+        }
+        return k - 1;
+    }
+
+    protected int twoK(int k) {
+        int result = 1;
+        for (int i = 0; i < k; i++) result *= 2;
+        return result;
+    }
+
     @Override
     public void preProcess(int[] arr) {
         n = arr.length;
@@ -16,7 +36,6 @@ public class RMQSparseTableIndex extends RMQSparseTable {
         }
     }
 
-    @Override
     public int query(int[] arr, int i, int j) {
         int k = highestK(j - i + 1);
         return Math.min(arr[st[i][k]], arr[st[j - twoK(k) + 1][k]]);
